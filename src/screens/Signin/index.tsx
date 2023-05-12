0bd9757fd774e6d0798cf455e9c60c1deab7cf2f
Carlos Alves <carlosaalves07@outlook.com>
2023-05-12 11:18:13 -0300

    feat: first commit

---
+++ src/screens/Signin/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+import { styles } from "./styles";
+
+export function Signin() {
+  return (
+    <View style={styles.container}>
+      <Text>Olá</Text>
+    </View>
+  );
+}
